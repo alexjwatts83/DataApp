7a0cffb65600b477daf654ee4967a0d04f5f410f
alexjwatts83 <alexjwatts83@gmail.com>
2021-06-17 22:29:40 +1000

    Added entity framework

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
