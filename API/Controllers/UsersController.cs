e91b355d32d1e68cc90c77e0d43fddc0d6adfa29
alexjwatts83 <alexjwatts83@gmail.com>
2021-06-18 09:43:46 +1000

    sync

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public UsersController(DataContext context, ILogger<WeatherForecastController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // api/user
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> Get()
+        {
+            return _context.Users.ToList();
+        }
+
+        // api/users/1
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetById(int id)
+        {
+            return _context.Users.Find(id);
+        }
+    }
+}
