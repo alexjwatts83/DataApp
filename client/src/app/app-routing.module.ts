7e7d0ffaf573ab5c8a47bbf53c83fbb0cf1f52b4
alexjwatts83 <alexjwatts83@gmail.com>
2021-06-23 20:57:48 +1000

    added simple authguard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { ListsComponent } from './lists/lists.component';
 import { MemberDetailsComponent } from './members/member-details/member-details.component';
 import { MembersListComponent } from './members/members-list/members-list.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guard/auth.guard';
 
 const routes: Routes = [
   {
     path: '', component: HomeComponent
   },
   {
-    path: 'members', component: MembersListComponent
+    path: 'members', component: MembersListComponent, canActivate: [AuthGuard]
   },
   {
     path: 'members/:id', component: MemberDetailsComponent
