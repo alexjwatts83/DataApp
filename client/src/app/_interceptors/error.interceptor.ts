7539512d57c52bc175f6c0fbbc1abdf047709182
alexjwatts83 <alexjwatts83@gmail.com>
2021-06-24 21:58:05 +1000

    Added interceptor

---
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private router: Router, private toastr: ToastrService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError(error => {
+        if(error) {
+          // console.log('intercept error caught', error)
+          switch (error.status) {
+            case 400:
+              if (error.error.errors) {
+                // console.log('has error.error.errors', error)
+                const modelStateErrors = [];
+                for(const key in error.error.errors){
+                  if (error.error.errors[key]) {
+                    modelStateErrors.push(error.error.errors[key]);
+                  }
+                }
+                throw modelStateErrors;
+              } else {
+                this.toastr.error(error.statusText, error.status);
+              }
+              break;
+            case 401:
+              this.toastr.error(error.statusText, error.status);
+              break;
+            case 403:
+              // TODO: add something here ah
+              break;
+            case 404:
+              this.router.navigateByUrl('/not-found');
+              break;
+            case 500:
+              const navigationExtras: NavigationExtras =  {state: {error: error.error}}
+              this.router.navigateByUrl('/server-errors', navigationExtras);
+              break;
+            default:
+              this.toastr.error('An error occured, lols');
+              console.log('intercept', error);
+              break;
+          }
+        }
+        return throwError(error)
+      }));
+  }
+}
