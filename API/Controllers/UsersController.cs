432d50b91b26bc4ef54e676ab3a651da7a2d1bec
alexjwatts83 <alexjwatts83@gmail.com>
2021-06-29 19:53:34 +1000

    User controller returns memberdto

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Interface;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
@@ -17,26 +19,31 @@ namespace API.Controllers
     {
         private readonly IUserRepository _userRepository;
         private readonly ILogger<UsersController> _logger;
+        private readonly IMapper _mapper;
 
-        public UsersController(IUserRepository userRepository, ILogger<UsersController> logger)
+        public UsersController(IUserRepository userRepository, ILogger<UsersController> logger, IMapper mapper)
         {
             _userRepository = userRepository;
             _logger = logger;
+            _mapper = mapper;
         }
 
         // api/users
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> Get()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> Get()
         {
             var users = await _userRepository.GetUsersAsync().ConfigureAwait(false);
-            return Ok(users);
+            var memberDtos = _mapper.Map<IEnumerable<MemberDto>>(users);
+            return Ok(memberDtos);
         }
 
         // api/users/elle
         [HttpGet("{username}")]
-        public async Task<ActionResult<AppUser>> GetById(string username)
+        public async Task<ActionResult<MemberDto>> GetById(string username)
         {
-            return await _userRepository.GerUserByUsernameAsync(username).ConfigureAwait(false);
+            var user = await _userRepository.GerUserByUsernameAsync(username).ConfigureAwait(false);
+            var memberDto = _mapper.Map<MemberDto>(user);
+            return memberDto;
         }
     }
 }
--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -6,7 +6,11 @@ namespace API.DTOs
     public class MemberDto
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
+
+        public string PhotoUrl { get; set; }
+
+        // Automapper detects that there is a method named GetAge and uses then to set the Age
         public int Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
