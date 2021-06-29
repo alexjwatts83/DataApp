0db1ffbed3b2cf607e63ad169cf4cc42eac8f734
alexjwatts83 <alexjwatts83@gmail.com>
2021-06-29 20:41:15 +1000

    tidy up

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -7,10 +7,7 @@ namespace API.DTOs
     {
         public int Id { get; set; }
         public string Username { get; set; }
-
         public string PhotoUrl { get; set; }
-
-        // Automapper detects that there is a method named GetAge and uses then to set the Age
         public int Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using API.Extentions;
 
 namespace API.Entities
 {
@@ -21,7 +19,5 @@ namespace API.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
-
-        //public int GetAge() => DateOfBirth.CalculateAge();
     }
 }
