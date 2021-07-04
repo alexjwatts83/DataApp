2e3d0bd58eeccfae42b505c207d41953b3981631
alexjwatts83 <alexjwatts83@gmail.com>
2021-07-05 09:51:25 +1000

    using member service to mock storing of data

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../models/member';
 
@@ -8,6 +10,7 @@ import { Member } from '../models/member';
 })
 export class MembersService {
   private baseUrl = `${environment.apiUrl}/users`;
+  members: Member[] = [];
   constructor(private http: HttpClient) { 
   }
 
@@ -16,14 +19,31 @@ export class MembersService {
   }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl);
+    if(this.members.length > 0) {
+      return of(this.members);
+    }
+    return this.http
+      .get<Member[]>(this.baseUrl)
+      .pipe(map(members=> {
+        this.members = members;
+        return members;
+      }));
   }
 
   getMember(username: string) {
+    const member =  this.members.find(x => x.username === username);
+    if(member != null) {
+      return of(member);
+    }
     return this.http.get<Member>(this.getUrl(username));
   }
 
   updateMember(member: Member) {
-    return this.http.put(this.baseUrl, member);
+    return this.http
+      .put(this.baseUrl, member)
+      .pipe(map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      }));
   }
 }
--- client/src/app/members/members-list/members-list.component.ts
+++ client/src/app/members/members-list/members-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Member } from 'src/app/models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -8,19 +9,12 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./members-list.component.scss']
 })
 export class MembersListComponent implements OnInit {
-  members: Member[];
+  members$!: Observable<Member[]>;
   constructor(private memberService: MembersService) {
-    this.members = [];
+    
    }
 
   ngOnInit(): void {
-    this.loadMembers();
-  }
-
-  loadMembers() {
-    this.memberService.getMembers().subscribe(members=>{
-      this.members = members;
-      console.log('members', this.members);
-    });
+    this.members$ = this.memberService.getMembers();
   }
 }
