71f26b3f0dc022648642e3776c4068f6f4559dc8
alexjwatts83 <alexjwatts83@gmail.com>
2021-07-07 07:23:43 +1000

    cleaned up code after the many attempts in trying to get the validator working.

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import {
   AbstractControl,
-  FormBuilder,
   FormControl,
   FormGroup,
   ValidationErrors,
   ValidatorFn,
   Validators,
 } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -18,36 +16,34 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter<boolean>();
+
   model: any = {};
   registerForm!: FormGroup;
-  // public form: FormGroup;
-  constructor(
-    private accounterService: AccountService,
-    private toastr: ToastrService,
-    private fb: FormBuilder
-  ) {
-    this.registerForm = new FormGroup({
-      username: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl('', [Validators.required, this.matchValuesValidator('password') ])
-    },
-    // this.passwordsShouldMatch
-  );
+
+  constructor(private accounterService: AccountService) {
+
   }
 
   ngOnInit(): void {
-    // this.initForm();
+    this.initForm();
   }
 
-  private checkPassword(control: FormControl) {
-    return control.value.toString().length >= 5 && control.value.toString().length <= 10
-      ? null
-      : {'outOfRange': true};
-  }
-
-  private passwordsShouldMatch(fGroup: FormGroup) {
-    return fGroup?.get('password')?.value === fGroup?.get('passwordConfirm')?.value
-      ? null : {'mismatch': true};
+  initForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(8),
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        this.matchValuesValidator('password'),
+      ]),
+    });
+    this.registerForm.controls.password.valueChanges.subscribe(() => {
+      this.registerForm.controls.confirmPassword.updateValueAndValidity();
+    });
   }
 
   matchValuesValidator(matchTo: string): ValidatorFn {
@@ -56,10 +52,7 @@ export class RegisterComponent implements OnInit {
       const password = parent?.get(matchTo)?.value;
       const confirmPassword = control.value;
       const matched = password === confirmPassword;
-      // console.clear();
-      // console.log({password:password,confirmPassword:confirmPassword, matched: matched });
-
-      return matched ? null : {isMatched: {value: control.value}};
+      return matched ? null : { isMatched: { value: control.value } };
     };
   }
 
