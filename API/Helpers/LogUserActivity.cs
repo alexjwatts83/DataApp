17b50382a53ee0886cde879243462fbd16bfa561
alexjwatts83 <alexjwatts83@gmail.com>
2021-07-09 08:00:25 +1000

    Added ActionFilter to log users last active date

---
+++ API/Helpers/LogUserActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using API.Extentions;
+using API.Interface;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultContenxt = await next().ConfigureAwait(false);
+
+            if(!resultContenxt.HttpContext.User.Identity.IsAuthenticated)
+            {
+                return;
+            }
+
+            var username = resultContenxt.HttpContext.User.GetUsername();
+            var respository = resultContenxt.HttpContext.RequestServices.GetService<IUserRepository>();
+            var user = await respository.GerUserByUsernameAsync(username).ConfigureAwait(false);
+
+            user.LastActive = DateTime.Now;
+
+            await respository.SallAllAsync();
+        }
+    }
+}
