f7120f89c3951cde3912a0fa58f1daf9e8a866bc
alexjwatts83 <alexjwatts83@gmail.com>
2021-07-10 08:53:46 +1000

    setup for ILikesRepository

---
+++ API/Interface/ILikesRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Helpers;
+
+namespace API.Interface
+{
+    public interface ILikesRepository
+    {
+        Task<UserLike> GetUserLike(int sourceUserId, int likedUserdId);
+        Task<AppUser> GetUserWithLikes(int userId);
+        Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId);
+    }
+
+}
