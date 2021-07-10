6547e9848102ecff0563abf92176f7d905eb9fd0
alexjwatts83 <alexjwatts83@gmail.com>
2021-07-10 20:24:15 +1000

    Added message entity migration to db but have not updated it yet

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -13,6 +13,7 @@ namespace API.Data
         public DbSet<AppUser> Users { get; set; }
 
         public DbSet<UserLike> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -37,6 +38,20 @@ namespace API.Data
                 .WithMany(l => l.LikedByUsers)
                 .HasForeignKey(s => s.LikedUserId)
                 .OnDelete(DeleteBehavior.Cascade);
+
+            // recpient has many messages received
+            builder
+                .Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(m => m.MessagesReceived)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // sender has many messages sent
+            builder
+                .Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(m => m.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
