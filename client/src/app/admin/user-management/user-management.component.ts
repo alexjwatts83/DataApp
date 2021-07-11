4a3890473af09b785f375125f010827bce740298
alexjwatts83 <alexjwatts83@gmail.com>
2021-07-11 21:32:10 +1000

    Added some admin components

---
+++ client/src/app/admin/user-management/user-management.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { AdminService } from 'src/app/_services/admin.service';
+
+@Component({
+  selector: 'app-user-management',
+  templateUrl: './user-management.component.html',
+  styleUrls: ['./user-management.component.scss']
+})
+export class UserManagementComponent implements OnInit {
+  users!: Partial<User[]> | undefined;
+  
+  constructor(private adminService: AdminService) { }
+
+  ngOnInit(): void {
+    this.getUsersWithRoles();
+  }
+
+  getUsersWithRoles() {
+    this.adminService.getUsersWithRoles().subscribe(users => {
+      this.users = users;
+    });
+  }
+}
