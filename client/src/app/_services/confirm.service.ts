71d9363fc3eadd47132c727fa68d79e355256d6a
alexjwatts83 <alexjwatts83@gmail.com>
2021-07-14 22:49:42 +1000

    finished off confirm service and added it to the prevent unsaved changes guard.

--- client/src/app/_services/confirm.service.ts
+++ client/src/app/_services/confirm.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { Observable } from 'rxjs';
+import { ConfirmDialogComponent } from '../modals/confirm-dialog/confirm-dialog.component';
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +15,7 @@ export class ConfirmService {
     message = 'Are you sure you want to do this?',
     btnOkText = 'Ok',
     btnCancelText = 'Cancel'
-  ) {
+  ): Observable<boolean> {
     const config = {
       class: 'modal-dialog-centered',
       initialState: {
@@ -24,6 +26,22 @@ export class ConfirmService {
       },
     };
 
-    this.bsModalRef = this.modalService.show('confirm', config);
+    this.bsModalRef = this.modalService.show(ConfirmDialogComponent, config);
+    return new Observable<boolean>(this.getResult());
+  }
+
+  private getResult() {
+    return (observer: any) => {
+      const subscription = this.bsModalRef.onHidden.subscribe(() => {
+        observer.next(this.bsModalRef.content.result);
+        observer.complete();
+      });
+
+      return {
+        unsubscribe() {
+          subscription.unsubscribe();
+        }
+      }
+    }
   }
 }
