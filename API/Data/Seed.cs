2d43b6b192ad3a6870a605b81589a8956bbe3f4a
alexjwatts83 <alexjwatts83@gmail.com>
2021-07-15 16:17:39 +1000

    updated seed again

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -65,9 +65,6 @@ namespace API.Data
             await userManager
                 .AddToRolesAsync(admin, new[] { "Admin", "Moderator" })
                 .ConfigureAwait(false);
-
-            var rolesFromManager = await roleManager.Roles.ToListAsync().ConfigureAwait(false);
-            var adminName = await roleManager.Roles.FirstOrDefaultAsync(x => x.Name.ToLower() == "admin");
         }
     }
 }
